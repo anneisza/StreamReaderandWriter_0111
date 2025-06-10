940defabd85f76c8f0e049b121e20a9188666f12
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-10 17:37:30 +0700

    Melakukan perulangan

--- ioFile.cpp
+++ ioFile.cpp
@@ -26,6 +26,17 @@ int main(){
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan membaca file"<<endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(inline, baris))
+        {
+            /* code */
+        }
+        
+    }
+    
     
 
 }
