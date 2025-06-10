5235dcbf591135045dec6c5639fcdfd6a7e1734d
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-10 17:47:42 +0700

    Memperbaiki letak koma atas

--- ioFile.cpp
+++ ioFile.cpp
@@ -9,7 +9,7 @@ int main(){
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
-    cout<< ">= Menulis file \'q'\ untuk keluar"<<endl;
+    cout<< ">= Menulis file \'q\' untuk keluar"<<endl;
 
     while (true)
     {
