27b8e4ee255501fda3403937503f6b667d3dded0
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-10 18:46:18 +0700

    Membuat program utama berisi manual exception

---
+++ manualException.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    try
+    {
+        cout<< "Selamat belajar di Prodi TI UMY"<<endl;
+        throw 0.5;//melemparkan sebuah integer maka
+        cout<< "Pernyataan tidak akan dieksekusi"<<endl;
+    }
+    catch(int a)
+    {
+        //blok ini akan dieksekusi
+        cout<< "Pengecualian akan dieksekusi"<<endl;
+    }
+    
+}
