1859349cbc5eb80e3e2ef79184fcbe31d1b57f75
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-10 17:46:30 +0700

    Memperbaiki kode yang belum ditulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -5,24 +5,26 @@ using namespace std;
 
 int main(){
     string baris;
-    //membuaka file dalam mode menulis
+    //membuka file dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+    cout<< ">= Menulis file \'q'\ untuk keluar"<<endl;
 
     while (true)
     {
-        cout<< "-"
+        cout<< "-";
         //mendapatkan karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q") break;
         //menulis dan memasukkan nilai dari baris ke dalam file
-        outfile << baris << endl;
-        
+        outfile << baris << endl;   
     }
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
     //
+    ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan membaca file"<<endl;
@@ -30,7 +32,7 @@ int main(){
     if (infile.is_open())
     {
         //melakukan perulangan setiap baris
-        while (getline(inline, baris))
+        while (getline(infile, baris))
         {
             //dan tampilkan di sini
             cout<< baris << '\n';
