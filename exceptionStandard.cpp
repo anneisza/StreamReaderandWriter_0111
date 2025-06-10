f5e8960e76348faac2d2b4572e877e5b53c0250b
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-10 18:39:32 +0700

    Melengkapi catch dan penanda 2

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -14,9 +14,14 @@ int main(){
         cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
     }
-    catch(const std::exception& e)
+    catch(exception& e)
     {
-        std::cerr << e.what() << '\n';
+        //penangkapan menggunakan obyek exception
+        cout<< e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 elemen*/
     }
-    
+    cout<< "Baris program yang terakhir" <<endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti
+    meskipun terjadi kesalahan*/
+    return 0;
 }
